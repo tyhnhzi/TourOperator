350ae000daa85490cc0c97077d685ce607e2ea2a
Hoang Pham <159236312+tyhnhzi@users.noreply.github.com>
2026-03-18 00:58:08 +0700

    Delete UploadGitHub directory

--- UploadGitHub/TourOperatorWeb/Models/VaiTro.cs
+++
@@ -1,9 +0,0 @@
-namespace TourOperatorWeb.Models;
-
-public class VaiTro
-{
-    public int Id { get; set; }
-    public string TenVaiTro { get; set; } = string.Empty; // VD: QuanTriHeThong, NhanVienDieuHanhTour
-
-    public ICollection<NguoiDung> NguoiDungs { get; set; } = new List<NguoiDung>();
-}

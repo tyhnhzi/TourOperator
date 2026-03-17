ac9a5a40bf26d89896b93f5d26722d07d63016bc
Hoang Pham <159236312+tyhnhzi@users.noreply.github.com>
2026-03-18 05:28:42 +0700

    Set application port from environment variable

--- TourOperatorWeb/Program.cs
+++ TourOperatorWeb/Program.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using TourOperatorWeb.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000"; 
+builder.WebHost.UseUrls($"http://*:{port}");
 // Add services to the container.
 builder.Services.AddRazorPages();
 
